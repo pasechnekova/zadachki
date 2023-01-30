29d5228379fd06cbf88a6bc595fd826a4282a1bb
Ирина Бакунович <pasechnekova@mail.ru>
2023-01-30 13:46:12 +0300

    Решение  38

--- zadachka36/Program.cs
+++ zadachka36/Program.cs
@@ -5,43 +5,44 @@
 
 Console.Clear();
 
-int size = ReadInt("Введите размерность массива: ");
-int min = ReadInt("Введите min число массива: ");
-int max = ReadInt("Введите max число массива: ");
-int [] numbers = new int[size];
-
+Console.WriteLine("Введите размер массива  ");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] numbers = new double[size];
 FillArrayRandomNumbers(numbers);
+Console.WriteLine("массив: ");
 PrintArray(numbers);
-int result = 0;
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
 
-for (int i = 0; i < numbers.Length; i++)
+for (int z = 0; z < numbers.Length; z++)
 {
-    if (i % 2 != 0)
-    {
-        result += numbers[i];
-    }
-}
-Console.WriteLine($"Сумма элементов нечетных позиций = {result}");
-
-void FillArrayRandomNumbers(int [] array) 
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        array[i] = new Random().Next(min, max);
-    }
+    if (numbers[z] > max)
+        {
+            max = numbers[z];
+        }
+    if (numbers[z] < min)
+        {
+            min = numbers[z];
+        }
 }
 
-void PrintArray(int[] array) 
+Console.WriteLine($"всего {numbers.Length} чисел. max значение = {max}, min значение = {min}");
+Console.WriteLine($"Разница между max и min значением = {max - min}");
+
+void FillArrayRandomNumbers(double[] numbers)
 {
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write(array[i] + " ");
-    }
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
+        }
+}
+void PrintArray(double[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
     Console.WriteLine();
-}
-
-int ReadInt(string message)  
-{
-    Console.Write(message);
-    return Convert.ToInt32(Console.ReadLine());
 }
