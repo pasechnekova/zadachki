623de77791345cb65dc51bdaa5d604d8ff712986
Ирина Бакунович <pasechnekova@mail.ru>
2023-02-10 14:04:10 +0300

    решение  60

--- zadachka60/Program.cs
+++ zadachka60/Program.cs
@@ -9,4 +9,60 @@
 
 Console.Clear();
 
+int[,,] CreateMatrix(int row, int col, int dep, int min, int max)
+{
+    int[,,] matrix = new int[row, col, dep];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++) { matrix[i, j, k] = GetUniqueValue(matrix, min, max, i, j, k); }
+        }
+    }
+    return matrix;
+}
+Random rnd = new Random();
+int GetUniqueValue(int[,,] matrix, int min, int max, int i, int j, int k)
+{
+    int value = default;
+    bool exist = true;
+    while (exist)
+    {
+        bool _break = false;
+        value = rnd.Next(min, max + 1);
+        for (int i1 = 0; i1 < matrix.GetLength(0); i1++)
+        {
+            if (_break) { break; }
+            for (int j1 = 0; j1 < matrix.GetLength(1); j1++)
+            {
+                if (_break) { break; }
+                for (int k1 = 0; k1 < matrix.GetLength(2); k1++)
+                {
+                    if (matrix[i1, j1, k1] == value) { _break = true; break; }
+                    if (i1 == i && j1 == j && k1 == k) { exist = false; }
+                }
+            }
+        }
+    }
+    return value;
+}
 
+void PrintMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write("|");
+            for (int k = 0; k < matrix.GetLength(2); k++) { Console.Write($"{matrix[i, j, k],1}({i},{j},{k})|"); }
+            Console.WriteLine();
+        }
+    }
+}
+
+void Main(string[] args)
+{
+    int[,,] array3D = CreateMatrix(2, 2, 2, 10, 99);
+    PrintMatrix(array3D);
+    Console.ReadLine();
+}
