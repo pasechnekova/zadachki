ef098d0075bef1e43a27c0debaaee8889636e015
Ирина Бакунович <pasechnekova@mail.ru>
2023-02-12 12:54:56 +0300

    решение  66

--- zadachka66/Program.cs
+++ zadachka66/Program.cs
@@ -5,3 +5,22 @@ M = 4; N = 8. -> 30      */
 
 Console.Clear();
 
+Console.WriteLine("Введите начальное число M:");
+int numberM = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите начальное число M:");
+int numberN = int.Parse(Console.ReadLine());
+
+///Метод нахождения суммы натуральных элементов в промежутке от M до N
+void GetNumberSum (int numberM, int numberN, int sum)
+{
+    if (numberM > numberN) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (numberM++);
+    GetNumberSum(numberM, numberN, sum);
+}
+
+GetNumberSum(numberM, numberN, 0);
