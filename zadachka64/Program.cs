3900f7eacb3d1e175142ceba36b4d23ee105939c
Ирина Бакунович <pasechnekova@mail.ru>
2023-02-12 12:46:33 +0300

    решение  64

--- zadachka64/Program.cs
+++ zadachka64/Program.cs
@@ -6,4 +6,15 @@ N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" */
 
 Console.Clear();
 
+Console.WriteLine("Введите натуральное число больше 1:");
+int number = int.Parse(Console.ReadLine());
 
+void NumberCounter (int number)
+{
+    if (number < 0) Console.Write($"{number} не натуральное число");
+    if (number == 0) return;
+    Console.Write("{0,4}", number);
+    NumberCounter (number - 1);
+}
+
+NumberCounter(number);
