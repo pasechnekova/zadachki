a42a0d15031c664cc40a57464d75cfcc07ec4ce6
Ирина Бакунович <pasechnekova@mail.ru>
2023-02-12 13:02:55 +0300

    решение  68

--- zadachka68/Program.cs
+++ zadachka68/Program.cs
@@ -5,4 +5,18 @@ m = 3, n = 2 -> A(m,n) = 29  */
 
 Console.Clear();
 
+Console.WriteLine("Введите начальное число M:");
+int numberM = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите начальное число N:");
+int numberN = int.Parse(Console.ReadLine());
 
+///Метод вычисления функции Аккермана:
+int Function (int numberM, int numberN)
+{
+    if (numberM == 0) return numberN + 1;
+    if (numberM != 0 && numberN == 0) return Function(numberM - 1, 1);
+    if (numberM > 0 && numberN > 0) return Function(numberM - 1, Function(numberM, numberN - 1));
+return Function(numberM, numberN);
+}
+
+Console.WriteLine($"Функция  для чисел A({numberM},{numberN}) = {Function(numberM, numberN)}");
